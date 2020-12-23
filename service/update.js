9ec7c2eb1447765f82f03483448d806d623d6b1d
Torbjørn Dahl <torbjorndahl@Torbjrns-MacBook-Pro.local>
2020-12-23 14:28:30 +0100

    added gw 14 data

--- service/update.js
+++ service/update.js
@@ -1,14 +1,14 @@
-const root = require('app-root-path');
-const remote = require(root + '/service/helpers/getData');
-const preppers = require(root + '/service/helpers/preppingFunctions');
+const root = require("app-root-path");
+const remote = require(root + "/service/helpers/getData");
+const preppers = require(root + "/service/helpers/preppingFunctions");
 const { updateTopPlayerData } = require(root +
-  '/service/helpers/makeTopManagerStats');
+  "/service/helpers/makeTopManagerStats");
 
 async function update() {
   await remote.getData();
   const pages = 2;
   const league = 314;
-  const gw = 13;
+  const gw = 14;
   await updateTopPlayerData(pages, league, gw);
   await preppers.prepPlayers();
 }
