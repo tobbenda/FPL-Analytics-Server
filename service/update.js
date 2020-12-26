7c2bf3c6de2b3844eaf8e2883764a757cf9c24a2
Torbjørn Dahl <torbjorndahl@Torbjrns-MacBook-Pro.local>
2020-12-26 14:52:29 +0100

    test with data for gw 15 (only top manager own and cap will be avail)

--- service/update.js
+++ service/update.js
@@ -8,7 +8,7 @@ async function update() {
   await remote.getData();
   const pages = 2;
   const league = 314;
-  const gw = 14;
+  const gw = 15;
   await updateTopPlayerData(pages, league, gw);
   await preppers.prepPlayers();
 }
